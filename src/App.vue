0e2ed203ee44acd5b094cbf125edddae6573bd6f
Onurcan <113804707+onrcn-tnrkl@users.noreply.github.com>
2025-09-12 09:30:25 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
           alt="Logo"
           class="w-24 h-24 mb-2 rounded-full object-cover shadow-md"
         />
-        <h1 class="text-xl font-semibold text-gray-800">Pams Menu</h1>
+        <h1 class="text-xl font-semibold text-gray-800">Pams No : 49</h1>
       </div>
     </header>
 
